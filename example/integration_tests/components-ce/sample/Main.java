852316d970b9bc5a0ca3126b881be57eb89931e4
Sam Gammon <sam@elide.ventures>
2023-09-03 16:17:30 -0700

    test: extend `components-ce` to test `js` use
    - test: add code to `components-ce` test to ensure `js`
      component is installed and usable
    
    Co-authored-by: Mantas Indrašius <mantasi@wix.com>
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- example/integration_tests/components-ce/sample/Main.java
+++ example/integration_tests/components-ce/sample/Main.java
@@ -1,5 +1,17 @@
+import org.graalvm.polyglot.Context;
+
+import java.io.BufferedOutputStream;
+import java.io.OutputStream;
+
+
 public class Main {
     public static void main(String args[]) {
-        System.out.println("Hello, GraalVM!");
+        OutputStream myOut = new BufferedOutputStream(System.out);
+        Context context = Context.newBuilder("js")
+                .out(myOut)
+                .allowAllAccess(true)
+                .build();
+        context.eval("js", "42");
+        context.close();
     }
 }
