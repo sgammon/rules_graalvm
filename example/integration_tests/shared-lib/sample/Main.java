a7fff83cbea5b0dea6e37edf509b0aebbaa64bc7
Sam Gammon <sam@elide.dev>
2026-04-26 10:07:54 -0700

    fix: shared library test has no entrypoints
    Signed-off-by: Sam Gammon <sam@elide.dev>

--- example/integration_tests/shared-lib/sample/Main.java
+++ example/integration_tests/shared-lib/sample/Main.java
@@ -1,5 +1,13 @@
+import org.graalvm.nativeimage.IsolateThread;
+import org.graalvm.nativeimage.c.function.CEntryPoint;
+
 public class Main {
     public static void main(String args[]) {
         System.out.println("Hello, GraalVM!");
+    }
+
+    @CEntryPoint(name = "sample_meaning")
+    public static int meaning(IsolateThread thread) {
+        return 42;
     }
 }
